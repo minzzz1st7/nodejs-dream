95ca7629b56e753301a907090aa26452c317ebcb
minzzz1st7 <minzzz1st7@gmail.com>
2021-09-29 23:58:48 +0900

    a green lawn

--- 8-validation/app.js
+++ 8-validation/app.js
@@ -4,7 +4,14 @@ import { body, validationResult } from 'express-validator';
 const app = express();
 app.use(express.json());
 
-app.post('/users', body('name').isLength({min: 2, max: 10}), (req, res, next) => {
+app.post(
+    '/users',
+    body('name')
+        .notEmpty()
+        .withMessage('이름을 입력해')
+        .isLength({min: 2, max: 10})
+        .withMessage('이름은 두 글자 이상'),
+    (req, res, next) => {
         const errors = validationResult(req);
         if(!errors.isEmpty()) {
             res.status(400).json({ message: errors.array() });
@@ -18,4 +25,4 @@ app.get('/:email', (req, res, next) => {
 
 })
 
-app.listen(8000);
+app.listen(8080);
