54f4cd4243538ead75ed73b74b8757f603d635d1
minzzz1st7 <minzzz1st7@gmail.com>
2021-09-30 23:26:03 +0900

    a green lawn

--- 8-validation/app.js
+++ 8-validation/app.js
@@ -1,27 +1,38 @@
 import express from 'express';
-import { body, validationResult } from 'express-validator';
+import { body, param, validationResult } from 'express-validator';
 
 const app = express();
 app.use(express.json());
 
+
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+    if(errors.isEmpty()) {
+        return next();
+    }
+    return res.status(400).json({ message: errors.array()[0].msg});
+};
+
 app.post(
     '/users',
-    body('name')
-        .notEmpty()
-        .withMessage('이름을 입력해')
-        .isLength({min: 2, max: 10})
-        .withMessage('이름은 두 글자 이상'),
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()) {
-            res.status(400).json({ message: errors.array() });
-        }
+    [
+        body('name').isLength({min: 2, max: 10}).withMessage('이름은 두 글자 이상'),
+        body('age').notEmpty().isInt().withMessage('숫자를 입력해'),
+        body('email').isEmail().withMessage('이메일을 입력해요'),
+        body('job.name').notEmpty,
+        validate
+    ],
+    (req, res, next) => { 
     console.log(req.body);
     res.sendStatus(201);
 });
 
-app.get('/:email', (req, res, next) => {
+app.get(
+    '/:email', 
+    [param('email').isEmail().withMessage('이메일 입력'),validate],
+    (req, res, next) => {
     res.send('goooooooooood');
+    
 
 })
 
